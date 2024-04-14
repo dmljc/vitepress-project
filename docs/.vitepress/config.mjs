b79385cc9c97078036ed0a54b31cd2799df7df3f
zfc <17602143402@163.com>
2024-04-14 18:01:16 +0800

    feat:# 光源的阴影相机属性阴影范围 .shadow.camera

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -9,144 +9,250 @@ export default defineConfig({
     head: [["link", { rel: "icon", href: "./favicon.ico" }]],
     lastUpdated: true,
     markdown: {
-        lineNumbers: true
+        lineNumbers: true,
     },
     themeConfig: {
-        logo: "/logo.png", 
+        logo: "/logo.png",
         nav: [
             { text: "首页", link: "/" },
             { text: "3D可视化", link: "/threejs/index" },
         ],
         sidebar: {
             // 当用户位于 `threejs` 目录时，会显示此侧边栏
-            '/threejs/': [
+            "/threejs/": [
                 {
-                    text: '基础概念',
+                    text: "基础概念",
                     collapsed: true,
                     items: [
-                        { text: '虚拟场景 Scene', link: '/threejs/phaseA/scene' },
-                        { text: '虚拟相机 Camera', link: '/threejs/phaseA/camera' },
-                        { text: '渲染器 Renderer', link: '/threejs/phaseA/renderer' },
-                        { text: '坐标系 AxesHelper', link: '/threejs/phaseA/axesHelper' },
-                        { text: '光源 Light', link: '/threejs/phaseA/light' },
-                        { text: '轨道控制器 OrbitControls', link: '/threejs/phaseA/orbitControls' },
-                        { text: '动画 requestAnimationFrame', link: '/threejs/phaseA/requestAnimationFrame' },
-                        { text: '阵列立方体', link: '/threejs/phaseA/matrix' },
-                        { text: '渲染器设置(锯齿模糊)', link: '/threejs/phaseA/antialias' },
-                        { text: 'Gui 可视化改变三维场景', link: '/threejs/phaseA/gui' },
-                        { text: 'Gui 常用事件', link: '/threejs/phaseA/guiEvent' },
-                        { text: 'Gui 常用组件', link: '/threejs/phaseA/guiComp' },
-                        { text: 'Gui 分组管理', link: '/threejs/phaseA/guiGroup' },
-                    ]
+                        { text: "虚拟场景 Scene", link: "/threejs/phaseA/scene" },
+                        { text: "虚拟相机 Camera", link: "/threejs/phaseA/camera" },
+                        { text: "渲染器 Renderer", link: "/threejs/phaseA/renderer" },
+                        { text: "坐标系 AxesHelper", link: "/threejs/phaseA/axesHelper" },
+                        { text: "光源 Light", link: "/threejs/phaseA/light" },
+                        {
+                            text: "轨道控制器 OrbitControls",
+                            link: "/threejs/phaseA/orbitControls",
+                        },
+                        {
+                            text: "动画 requestAnimationFrame",
+                            link: "/threejs/phaseA/requestAnimationFrame",
+                        },
+                        { text: "阵列立方体", link: "/threejs/phaseA/matrix" },
+                        { text: "渲染器设置(锯齿模糊)", link: "/threejs/phaseA/antialias" },
+                        { text: "Gui 可视化改变三维场景", link: "/threejs/phaseA/gui" },
+                        { text: "Gui 常用事件", link: "/threejs/phaseA/guiEvent" },
+                        { text: "Gui 常用组件", link: "/threejs/phaseA/guiComp" },
+                        { text: "Gui 分组管理", link: "/threejs/phaseA/guiGroup" },
+                    ],
                 },
                 {
-                    text: '进阶概念',
+                    text: "进阶概念",
                     collapsed: true,
                     items: [
-                        { text: '三维对象 Vector3', link: '/threejs/phaseB/vector3' },
-                        { text: '欧拉对象 Euler', link: '/threejs/phaseB/euler' },
-                        { text: '模型材质颜色 Color 对象', link: '/threejs/phaseB/color' },
-                        { text: '模型材质父类 Material对象', link: '/threejs/phaseB/material' },
-                        { text: '克隆.clone()和复制.copy()', link: '/threejs/phaseB/clone' },
-                        { text: 'Group 层级模型', link: '/threejs/phaseB/group' },
-                        { text: '树结构模型', link: '/threejs/phaseB/tree' },
-                        { text: '本地坐标和世界坐标', link: '/threejs/phaseB/coordinates' },
-                        { text: '移除.remove()和显示隐藏visible', link: '/threejs/phaseB/remove' },
-                        { text: '顶点 UV 坐标、纹理贴图', link: '/threejs/phaseB/uv' },
-                        { text: '自定义顶点UV坐标', link: '/threejs/phaseB/customUV' },
-                        { text: '圆形平面设置纹理贴图', link: '/threejs/phaseB/circle' },
-                        { text: '矩形Mesh+背景透明png贴图', link: '/threejs/phaseB/opacity' },
-                        { text: 'UV 动画 offset', link: '/threejs/phaseB/uvOffset' },
-                    ]
+                        { text: "三维对象 Vector3", link: "/threejs/phaseB/vector3" },
+                        { text: "欧拉对象 Euler", link: "/threejs/phaseB/euler" },
+                        { text: "模型材质颜色 Color 对象", link: "/threejs/phaseB/color" },
+                        {
+                            text: "模型材质父类 Material对象",
+                            link: "/threejs/phaseB/material",
+                        },
+                        {
+                            text: "克隆.clone()和复制.copy()",
+                            link: "/threejs/phaseB/clone",
+                        },
+                        { text: "Group 层级模型", link: "/threejs/phaseB/group" },
+                        { text: "树结构模型", link: "/threejs/phaseB/tree" },
+                        { text: "本地坐标和世界坐标", link: "/threejs/phaseB/coordinates" },
+                        {
+                            text: "移除.remove()和显示隐藏visible",
+                            link: "/threejs/phaseB/remove",
+                        },
+                        { text: "顶点 UV 坐标、纹理贴图", link: "/threejs/phaseB/uv" },
+                        { text: "自定义顶点UV坐标", link: "/threejs/phaseB/customUV" },
+                        { text: "圆形平面设置纹理贴图", link: "/threejs/phaseB/circle" },
+                        {
+                            text: "矩形Mesh+背景透明png贴图",
+                            link: "/threejs/phaseB/opacity",
+                        },
+                        { text: "UV 动画 offset", link: "/threejs/phaseB/uvOffset" },
+                    ],
                 },
                 {
-                    text: 'Blender 3D 建模',
+                    text: "Blender 3D 建模",
                     collapsed: true,
                     items: [
-                        { text: 'Blender 基本介绍', link: '/threejs/phaseC/blender' },
-                        { text: 'Blender 基本操作', link: '/threejs/phaseC/operate' },
-                        { text: '三维模型gltf介绍', link: '/threejs/phaseC/3dModel' },
-                        { text: 'Threejs 加载.gltf文件', link: '/threejs/phaseC/gltfLoad' },
-                        { text: 'gltf 不同文件形式', link: '/threejs/phaseC/pattern' },
-                        { text: '纹理贴图错位', link: '/threejs/phaseC/attribute' },
-                    ]
+                        { text: "Blender 基本介绍", link: "/threejs/phaseC/blender" },
+                        { text: "Blender 基本操作", link: "/threejs/phaseC/operate" },
+                        { text: "三维模型gltf介绍", link: "/threejs/phaseC/3dModel" },
+                        { text: "Threejs 加载.gltf文件", link: "/threejs/phaseC/gltfLoad" },
+                        { text: "gltf 不同文件形式", link: "/threejs/phaseC/pattern" },
+                        { text: "纹理贴图错位", link: "/threejs/phaseC/attribute" },
+                    ],
                 },
                 {
-                    text: 'PBR 材质与纹理贴图',
+                    text: "PBR 材质与纹理贴图",
                     collapsed: true,
                     items: [
-                        { text: 'PBR 材质简介', link: '/threejs/phaseD/PBR' },
-                        { text: 'PBR 材质金属度和粗糙度', link: '/threejs/phaseD/metalness' },
-                        { text: '环境贴图.envMap（金属效果）', link: '/threejs/phaseD/envMap' },
-                        { text: '清漆层属性.clearcoat(车漆效果）', link: '/threejs/phaseD/clearcoat' },
-                        { text: '透光率.transmission和折射率.ior', link: '/threejs/phaseD/transmission' },
-                    ]
+                        { text: "PBR 材质简介", link: "/threejs/phaseD/PBR" },
+                        {
+                            text: "PBR 材质金属度和粗糙度",
+                            link: "/threejs/phaseD/metalness",
+                        },
+                        {
+                            text: "环境贴图.envMap（金属效果）",
+                            link: "/threejs/phaseD/envMap",
+                        },
+                        {
+                            text: "清漆层属性.clearcoat(车漆效果）",
+                            link: "/threejs/phaseD/clearcoat",
+                        },
+                        {
+                            text: "透光率.transmission和折射率.ior",
+                            link: "/threejs/phaseD/transmission",
+                        },
+                    ],
                 },
                 {
-                    text: 'Canvas 画布',
+                    text: "Canvas 画布",
                     collapsed: true,
                     items: [
-                        { text: 'Canvas 画布介绍', link: '/threejs/phaseE/basic.md' },
-                        { text: 'UI交互界面与Canvas画布叠加', link: '/threejs/phaseE/zIndex.md' },
-                        { text: 'UI交互按钮与3D场景交互', link: '/threejs/phaseE/click.md' },
-                        { text: 'Three.js背景透明度', link: '/threejs/phaseE/alpha.md' },
-                        { text: 'Three.js渲染结果保存为图片', link: '/threejs/phaseE/save.md' },
-                        { text: '深度冲突(模型闪烁)', link: '/threejs/phaseE/conflict.md' },
-                        { text: '模型加载进度条', link: '/threejs/phaseE/progress.md' },
-                    ]
+                        { text: "Canvas 画布介绍", link: "/threejs/phaseE/basic.md" },
+                        {
+                            text: "UI交互界面与Canvas画布叠加",
+                            link: "/threejs/phaseE/zIndex.md",
+                        },
+                        {
+                            text: "UI交互按钮与3D场景交互",
+                            link: "/threejs/phaseE/click.md",
+                        },
+                        { text: "Three.js背景透明度", link: "/threejs/phaseE/alpha.md" },
+                        {
+                            text: "Three.js渲染结果保存为图片",
+                            link: "/threejs/phaseE/save.md",
+                        },
+                        { text: "深度冲突(模型闪烁)", link: "/threejs/phaseE/conflict.md" },
+                        { text: "模型加载进度条", link: "/threejs/phaseE/progress.md" },
+                    ],
                 },
                 {
-                    text: '生成曲线和几何体',
+                    text: "生成曲线和几何体",
                     collapsed: true,
                     items: [
-                        { text: '生成圆弧顶点', link: '/threejs/phaseF/point.md' },
-                        { text: '几何体方法.setFromPoints()', link: '/threejs/phaseF/setFromPoints.md' },
-                        { text: '曲线Curve简介', link: '/threejs/phaseF/brief.md' },
-                        { text: '椭圆和圆弧线绘制', link: '/threejs/phaseF/ellipse.md' },
-                        { text: '2D、3D样条曲线', link: '/threejs/phaseF/spline.md' },
-                        { text: '贝塞尔曲线', link: '/threejs/phaseF/bezier.md' },
-                        { text: '贝塞尔曲线的应用', link: '/threejs/phaseF/application.md' },
-                        { text: '组合曲线CurvePath拼接曲线', link: '/threejs/phaseF/curvePath.md' },
-                        { text: '曲线路径管道TubeGeometry', link: '/threejs/phaseF/tube.md' },
-                        { text: '旋转成型LatheGeometry', link: '/threejs/phaseF/lathe.md' },
-                        { text: '旋转成型LatheGeometry', link: '/threejs/phaseF/lathe.md' },
-                        { text: '轮廓填充ShapeGeometry', link: '/threejs/phaseF/shape.md' },
-                        { text: '拉伸ExtrudeGeometry', link: '/threejs/phaseF/extrude.md' },
-                        { text: '扫描ExtrudeGeometry', link: '/threejs/phaseF/extrudePath.md' },
-                        { text: '多边形轮廓Shape简介', link: '/threejs/phaseF/shapePath.md' },
-                        { text: '多边形轮廓Shape(圆弧)', link: '/threejs/phaseF/shapeArc.md' },
-                        { text: '多边形Shape(内孔.holes)', link: '/threejs/phaseF/holes.md' },
-                        { text: '模型边界线EdgesGeometry', link: '/threejs/phaseF/edges.md' },
-                        { text: '几何体顶点颜色数数据', link: '/threejs/phaseF/pointColor.md' },
-                        { text: '一段曲线颜色渐变', link: '/threejs/phaseF/curveColor.md' },
-                        { text: 'Color颜色渐变插值', link: '/threejs/phaseF/lerpColors.md' },
-                        { text: '查看或设置gltf几何体顶点', link: '/threejs/phaseF/gltfPoints.md' },
-                        { text: '山脉地形高度可视化', link: '/threejs/phaseF/mountain.md' },
-                    ]
+                        { text: "生成圆弧顶点", link: "/threejs/phaseF/point.md" },
+                        {
+                            text: "几何体方法.setFromPoints()",
+                            link: "/threejs/phaseF/setFromPoints.md",
+                        },
+                        { text: "曲线Curve简介", link: "/threejs/phaseF/brief.md" },
+                        { text: "椭圆和圆弧线绘制", link: "/threejs/phaseF/ellipse.md" },
+                        { text: "2D、3D样条曲线", link: "/threejs/phaseF/spline.md" },
+                        { text: "贝塞尔曲线", link: "/threejs/phaseF/bezier.md" },
+                        {
+                            text: "贝塞尔曲线的应用",
+                            link: "/threejs/phaseF/application.md",
+                        },
+                        {
+                            text: "组合曲线CurvePath拼接曲线",
+                            link: "/threejs/phaseF/curvePath.md",
+                        },
+                        {
+                            text: "曲线路径管道TubeGeometry",
+                            link: "/threejs/phaseF/tube.md",
+                        },
+                        { text: "旋转成型LatheGeometry", link: "/threejs/phaseF/lathe.md" },
+                        { text: "旋转成型LatheGeometry", link: "/threejs/phaseF/lathe.md" },
+                        { text: "轮廓填充ShapeGeometry", link: "/threejs/phaseF/shape.md" },
+                        { text: "拉伸ExtrudeGeometry", link: "/threejs/phaseF/extrude.md" },
+                        {
+                            text: "扫描ExtrudeGeometry",
+                            link: "/threejs/phaseF/extrudePath.md",
+                        },
+                        {
+                            text: "多边形轮廓Shape简介",
+                            link: "/threejs/phaseF/shapePath.md",
+                        },
+                        {
+                            text: "多边形轮廓Shape(圆弧)",
+                            link: "/threejs/phaseF/shapeArc.md",
+                        },
+                        {
+                            text: "多边形Shape(内孔.holes)",
+                            link: "/threejs/phaseF/holes.md",
+                        },
+                        {
+                            text: "模型边界线EdgesGeometry",
+                            link: "/threejs/phaseF/edges.md",
+                        },
+                        {
+                            text: "几何体顶点颜色数数据",
+                            link: "/threejs/phaseF/pointColor.md",
+                        },
+                        { text: "一段曲线颜色渐变", link: "/threejs/phaseF/curveColor.md" },
+                        {
+                            text: "Color颜色渐变插值",
+                            link: "/threejs/phaseF/lerpColors.md",
+                        },
+                        {
+                            text: "查看或设置gltf几何体顶点",
+                            link: "/threejs/phaseF/gltfPoints.md",
+                        },
+                        { text: "山脉地形高度可视化", link: "/threejs/phaseF/mountain.md" },
+                    ],
                 },
                 {
-                    text: '相机基础',
+                    text: "相机基础",
                     collapsed: true,
                     items: [
-                        { text: '正投影相机', link: '/threejs/phaseG/orthographicCamera.md' },
-                        { text: '正投影相机-Canvas尺寸变化', link: '/threejs/phaseG/canvas.md' },
-                        { text: '包围盒Box3', link: '/threejs/phaseG/box3.md' },
-                        { text: '地图案例(包围盒、正投影)', link: '/threejs/phaseG/map.md' },
-                        { text: '相机动画(.position和.lookAt())', link: '/threejs/phaseG/animation.md' },
-                        { text: '不同方向的投影视图', link: '/threejs/phaseG/direction.md' },
-                        { text: '旋转渲染结果(.up相机上方向)', link: '/threejs/phaseG/up.md' },
-                        { text: '管道漫游案例', link: '/threejs/phaseG/tube.md' },
-                        { text: 'OrbitControls旋转缩放限制', link: '/threejs/phaseG/orbitControls.md' },
-                        { text: '相机控件 MapControls', link: '/threejs/phaseG/mapControls.md' },
-                    ]
+                        {
+                            text: "正投影相机",
+                            link: "/threejs/phaseG/orthographicCamera.md",
+                        },
+                        {
+                            text: "正投影相机-Canvas尺寸变化",
+                            link: "/threejs/phaseG/canvas.md",
+                        },
+                        { text: "包围盒Box3", link: "/threejs/phaseG/box3.md" },
+                        {
+                            text: "地图案例(包围盒、正投影)",
+                            link: "/threejs/phaseG/map.md",
+                        },
+                        {
+                            text: "相机动画(.position和.lookAt())",
+                            link: "/threejs/phaseG/animation.md",
+                        },
+                        {
+                            text: "不同方向的投影视图",
+                            link: "/threejs/phaseG/direction.md",
+                        },
+                        {
+                            text: "旋转渲染结果(.up相机上方向)",
+                            link: "/threejs/phaseG/up.md",
+                        },
+                        { text: "管道漫游案例", link: "/threejs/phaseG/tube.md" },
+                        {
+                            text: "OrbitControls旋转缩放限制",
+                            link: "/threejs/phaseG/orbitControls.md",
+                        },
+                        {
+                            text: "相机控件 MapControls",
+                            link: "/threejs/phaseG/mapControls.md",
+                        },
+                    ],
                 },
                 {
-                    text: '光源和阴影',
+                    text: "光源和阴影",
                     items: [
-                        { text: '聚光源SpotLight', link: '/threejs/phaseH/spotLight.md' },
-                        { text: '平行光阴影计算', link: '/threejs/phaseH/direcLightShadow.md' },
-                    ]
-                }
+                        { text: "聚光源SpotLight", link: "/threejs/phaseH/spotLight.md" },
+                        {
+                            text: "平行光阴影计算",
+                            link: "/threejs/phaseH/direcLightShadow.md",
+                        },
+                        {
+                            text: "阴影范围.shadow.camera",
+                            link: "/threejs/phaseH/shadow.md",
+                        },
+                    ],
+                },
             ],
         },
         socialLinks: [
@@ -157,7 +263,8 @@ export default defineConfig({
         },
         // 编辑链接
         editLink: {
-            pattern: "https://github.com/dmljc/vitepress-project/tree/main/docs/:path", // 自己项目仓库地址
+            pattern:
+                "https://github.com/dmljc/vitepress-project/tree/main/docs/:path", // 自己项目仓库地址
             text: "在 github 上编辑此页",
         },
         lastUpdatedText: "最后更新", // string
@@ -193,4 +300,3 @@ export default defineConfig({
         },
     },
 });
-
---
+++ docs/threejs/phaseH/shadow.md
@@ -0,0 +1,251 @@
+---
+outline: deep
+---
+
+# 光源的阴影相机属性 阴影范围 .shadow.camera
+
+上节课 2.平行光阴影计算讲解过，通过光源的阴影相机属性 `.shadow.camera`，来控制阴影的渲染范围，本节课继续.`shadow.camera` 的讲解。
+
+![shadows](/phaseH/shadows.jpg)
+
+## 平行光阴影相机属性 .shadow.camera
+
+- 平行光 `DirectionalLight` 的 `.shadow` 属性是平行光阴影对象 `DirectionalLightShadow`
+- 平行光阴影对象 `DirectionalLightShadow` 有一个相机属性 `.camera`
+- `.shadow.camera` 属性值(正投影相机 `OrthographicCamera`)
+
+## CameraHelper 可视化 .shadow.camera
+
+为了方便观察阴影渲染的范围，需要把平行光对应的阴影相机 `.shadow.camera` 可视化显示。
+
+```js
+new THREE.CameraHelper(directionalLight.shadow.camera);
+```
+
+## 参数测试
+
+下面先在上节课代码基础上给大家改参数体验下相机渲染范围参数对阴影的影响。
+
+课件演示文件中，提供了批量创建了多个长方体，用于阴影范围设置测试。
+
+## 设置相机 .shadow.camera 长方体范围
+
+根据 3D 场景渲染范围，去设置 `.shadow.camera` 长方体尺寸参数，一般比你要渲染的范围稍微大一些即可，过小阴影不显示或显示不完整，过大很多可能阴影偏模糊，你可以比较下面两个参数的阴影渲染差异。
+
+```js
+directionalLight.shadow.camera.left = -50 * 10;
+directionalLight.shadow.camera.right = 50 * 10;
+```
+
+```js
+directionalLight.shadow.camera.left = -50 * 100;
+directionalLight.shadow.camera.right = 50 * 100;
+```
+
+![正投影可视空间](/phaseH/正投影可视空间.png)
+
+## 调节光源位置
+
+光源位置影响平行光阴影相机 `.shadow.camera` 的位置，所以要根据渲染范围调整光源的位置。
+
+你可以比较测试两个不同的光源位置，对应阴影渲染效果。
+
+```js
+directionalLight.position.set(100, 60, 50);
+```
+
+```js
+directionalLight.position.set(100 * 2, 60 * 2, 50 * 2);
+```
+
+## 确定阴影计算范围
+
+其实平行光阴影范围的设置，你可以类比以前正投影机位置、长方体可视化空间的设置。
+
+- 先大概确定下 3D 场景中需要阴影计算范围，不用精确，有一个数量级就行，比如几百、几千。
+- `.shadow.camera` 的 `.left`、`.right`、`.top`、`.bottom`、`.near`、`.far` 属性定义的长方体空间
+- `.shadow.camera` 的位置(光源位置影响`.shadow.camera` 的位置)
+
+需要阴影范围数量级：z 方向尺寸 1000 左右，xy 方向 100 左右。
+
+```js
+for (let i = -3; i < 4; i++) {
+  const mesh2 = mesh.clone();
+  // 设置产生投影的网格模型
+  mesh2.castShadow = true;
+  mesh2.position.z = 100 * i;
+  group.add(mesh2);
+}
+```
+
+## 根据尺寸数量级设置阴影渲染范围
+
+比如光线是从斜上方照射下来，模型 y 方向高度 100 左右，这时候 y 可以设置为 100 左右，xz 也可以根据渲染范围先给个大概尺寸。
+
+```js
+directionalLight.position.set(100, 100, 100);
+// 平行光默认从.position指向坐标原点
+```
+
+光线方向，你可以改变 xz 坐标来调整
+
+```js
+directionalLight.position.set(-100, 100, -100);
+```
+
+渲染范围可以都先给个几百量级的值，不用精准，先设置，在微调。
+
+```js
+// 设置三维场景计算阴影的范围
+directionalLight.shadow.camera.left = -100;
+directionalLight.shadow.camera.right = 100;
+directionalLight.shadow.camera.top = 100;
+directionalLight.shadow.camera.bottom = -100;
+directionalLight.shadow.camera.near = 0.5;
+directionalLight.shadow.camera.far = 100;
+```
+
+## 批量创建阴影效果
+
+![shadowList](/phaseH/shadowList.jpg)
+
+## 示例代码如下：
+
+::: code-group
+
+```vue {26, 30-38} [index.vue]
+<template>
+  <div ref="lightDom"></div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from "vue";
+import * as THREE from "three";
+import model from "./model.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+const lightDom = ref(null);
+// 场景
+const scene = new THREE.Scene();
+scene.add(model);
+
+// 辅助观察坐标系
+const axesHelper = new THREE.AxesHelper(500);
+scene.add(axesHelper);
+
+// 环境光
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
+scene.add(ambientLight);
+
+// 平行光
+const directionalLight = new THREE.DirectionalLight(0xffffff, 4);
+directionalLight.position.set(200, 160, 150);
+scene.add(directionalLight);
+
+// 2、平行光设置产生阴影的光源对象，开启光源阴影的计算功能
+directionalLight.castShadow = true;
+
+// 5、设置三维场景计算阴影的范围
+directionalLight.shadow.camera.left = -50 * 6;
+directionalLight.shadow.camera.right = 50 * 6;
+directionalLight.shadow.camera.top = 200 * 2;
+directionalLight.shadow.camera.bottom = -100 * 2;
+directionalLight.shadow.camera.near = 0.5;
+directionalLight.shadow.camera.far = 400 * 10;
+
+// 可视化平行光阴影对应的正投影相机对象
+const cameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
+scene.add(cameraHelper);
+
+// 相机
+const width = window.innerWidth - 296;
+const height = window.innerHeight - 136;
+const camera = new THREE.PerspectiveCamera(30, width / height, 1, 3000);
+// camera.position.set(40, 122, 390);
+camera.position.set(153, 697, 676);
+camera.lookAt(0, 0, 0);
+
+// webgl 渲染器
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+});
+renderer.setSize(width, height);
+// 防止输出模糊
+renderer.setPixelRatio(window.devicePixelRatio);
+// 4、设置渲染器，允许光源阴影渲染
+renderer.shadowMap.enabled = true;
+
+// 相机轨道控制器
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.addEventListener("change", () => {
+  renderer.render(scene, camera);
+});
+
+// resize 事件会在窗口被调整大小时发生
+window.addEventListener("resize", () => {
+  const w = window.innerWidth - 296;
+  const h = window.innerHeight - 136;
+  renderer.setSize(w, h);
+  camera.aspect = w / h;
+  // 如果相机的一些属性发生了变化，需要执行 updateProjectionMatrix ()方法更新相机的投影矩阵
+  camera.updateProjectionMatrix();
+});
+
+onMounted(() => {
+  // renderer.setClearColor(0x444544, 0.4);
+  lightDom.value?.appendChild(renderer.domElement);
+});
+
+onUnmounted(() => {
+  renderer.dispose();
+});
+
+// 渲染循环
+const render = () => {
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+};
+render();
+</script>
+```
+
+```js {15, 28-34 } [model.js]
+import * as THREE from "three";
+
+// 立方体
+const geometry = new THREE.BoxGeometry(50, 100, 50);
+const material = new THREE.MeshLambertMaterial({
+  color: 0x00ffff,
+  side: THREE.DoubleSide,
+});
+const mesh = new THREE.Mesh(geometry, material);
+mesh.position.y = 50;
+// 1、设置产生投影的网格模型
+mesh.castShadow = true;
+
+// 平面
+const planeGeometry = new THREE.PlaneGeometry(400, 850);
+const planeMaterial = new THREE.MeshLambertMaterial({
+  color: 0x999999,
+  side: THREE.DoubleSide,
+});
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+planeMesh.rotateX(-Math.PI / 2);
+// 3、设置接收阴影的投影面
+planeMesh.receiveShadow = true;
+
+const group = new THREE.Group();
+group.add(mesh, planeMesh);
+
+for (let i = -3; i < 4; i++) {
+  const mesh2 = mesh.clone();
+  // 设置产生投影的网格模型
+  mesh2.castShadow = true;
+  mesh.position.z = i * 100;
+  group.add(mesh2);
+}
+
+export default group;
+```
+
+:::
